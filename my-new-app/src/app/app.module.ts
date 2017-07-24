cee9a8c0cd5d1b22075f0d700c59b666879fa415
Sara <seking13@gmail.com>
2017-07-24 14:36:02 -0600

    routesWorking

--- my-new-app/src/app/app.module.ts
+++ my-new-app/src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { JumboComponent } from './jumbo/jumbo.component';
 import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  {path: 'home', component: HomeComponent }
+  // { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
@@ -18,11 +26,17 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
 export class AppModule { }
