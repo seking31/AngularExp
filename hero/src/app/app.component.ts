c57c1c087721cc1f316736f17507c391b8a2f7b5
Sara <seking13@gmail.com>
2017-07-28 16:07:21 -0600

    newMod

--- hero/src/app/app.component.ts
+++ hero/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-  id:number;
-  name:string;
-}
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -24,19 +21,14 @@ const HEROES: Hero[] = [
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
 <ul class="heroes">
-  <li *ngFor = "let hero of heroes" (click)="onSelect(hero)">
+  <li *ngFor = "let hero of heroes" (click)="onSelect(hero)"
+  [class.selected]="hero === selectedHero">
   <span class = "badge"> {{hero.id}}</span>{{hero.name}}
   </li>
 </ul>
-
-<div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name">
-  </div>
-</div>`,
+<hero-detail [hero]='selectedHero'></hero-detail>
+`
+,
 styles: [`
   .selected {
     background-color: #CFD8DC !important;
--- hero/src/app/app.module.ts
+++ hero/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { HeroDetailComponent } from './hero-detail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
