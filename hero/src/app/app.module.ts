be94a5ab887347ac3ab8c33173c3ac50ff390047
Sara <seking13@gmail.com>
2017-07-31 14:22:39 -0600

    routing

--- hero/src/app/app.module.ts
+++ hero/src/app/app.module.ts
@@ -5,19 +5,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { HerosComponent } from './hero.component';
+
 import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
